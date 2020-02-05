b186505000430f434be342f00f738839056f4fa0
Aavesh Jilani <50949867+aaveshdev@users.noreply.github.com>
2020-02-05 20:42:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ echo "Dragon native 1.9.4"
 
 sudo cp dragon-native /bin/
 sudo chmod +x /bin/dragon-native
-sudo cp ./man/dragon-native.8 /usr/share/man/man8/dragon-native.8
-sudo gzip /usr/share/man/man8/dragon-native.8
+
 
 
 echo "Installed Successfully"
